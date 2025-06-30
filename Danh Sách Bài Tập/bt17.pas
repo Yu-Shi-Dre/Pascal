5e8f02b5b95f694547c1475ae1ba5683168f97b2
Nee Yu Shii <85595284+Yu-Shi-Dre@users.noreply.github.com>
2025-06-30 21:16:05 +0700

    Update bt17.pas

--- Danh Sách Bài Tập/bt17.pas
+++ Danh Sách Bài Tập/bt17.pas
@@ -4,7 +4,9 @@ uses crt;
 var a,b:real;
 begin
     clrscr;
-    writeln('Tim so lon hon trong hai so cho truoc.'+#13#10+#13#10,'Nhap hai so tu ban phim, lan luot la: ');readln(a);readln(b);
+    writeln('Tim so lon hon trong hai so cho truoc.');
+    writeln'Nhap hai so tu ban phim, lan luot la: ');
+    readln(a);readln(b);
     writeln;
     if a = b then write('Hai so bang nhau !');
     if a>b then write('So lon hon la: ',a:0:2);
