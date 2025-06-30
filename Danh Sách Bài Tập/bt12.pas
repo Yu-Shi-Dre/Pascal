e9a02807c42dd933790651818ac50db64b69a920
Nee Yu Shii <85595284+Yu-Shi-Dre@users.noreply.github.com>
2025-06-30 21:02:24 +0700

    Update bt12.pas

--- Danh Sách Bài Tập/bt12.pas
+++ Danh Sách Bài Tập/bt12.pas
@@ -4,12 +4,15 @@ uses crt;
 var a:real;
 begin
     clrscr;
-    write('Tinh chu vi va dien tich hinh vuong.'+#13#10+#13#10,'Nhap chieu dai canh hinh vuong: ');readln(a);
-    if a<0 then write(#13#10+'Ban da nhap sai so !');
-    if a>=0 then
+    writeln('Tinh chu vi va dien tich hinh vuong.');
+    writeln('Nhap chieu dai canh hinh vuong: ');readln(a);
+    writeln;
+    if a<=0 then write('Ban da nhap sai so !');
+    if a>0 then
     begin
        writeln;
-       write('Chu vi la: ',a*4:0:2,#13#10+'Dien tich la: ',a*a:0:2);
+       writeln('Chu vi la: ',a*4:0:2);
+       writeln('Dien tich la: ',a*a:0:2);
     end;
     readln
 end.
