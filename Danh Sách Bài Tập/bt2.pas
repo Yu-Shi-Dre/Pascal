0d48b2716ad594a6a352eac74c69ffb3e09684df
Nee Yu Shii <85595284+Yu-Shi-Dre@users.noreply.github.com>
2025-06-30 11:28:51 +0700

    Update bt2.pas

--- Danh Sách Bài Tập/bt2.pas
+++ Danh Sách Bài Tập/bt2.pas
@@ -7,10 +7,14 @@ begin
     writeln;
     writeln('Nhap 3 canh cua tam giac: ');readln(a,b,c);
     writeln;
-    if (a+b>c) and (a+c>b) and(b+c>a) then 
-    begin
-        if (a=b) or (a=c) or (b=c)  then write('Day la tam giac can !')
-        else write('Day khong phai tam giac can !');
+    if (a*b*c)>0 then
+    begin    
+      if (a+b>c) and (a+c>b) and(b+c>a) then 
+       begin
+          if (a=b) or (a=c) or (b=c)  then write('Day la tam giac can !')
+          else write('Day khong phai tam giac can !');
+       end
+       else write('Ban da nhap sai so. Vui long thuc hien lai chuong trinh !');
     end
     else write('Ban da nhap sai so. Vui long thuc hien lai chuong trinh !');
     readln
