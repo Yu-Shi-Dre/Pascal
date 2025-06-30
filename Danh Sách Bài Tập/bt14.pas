146a7c788c56933487ba701ad8174e179f872f22
Nee Yu Shii <85595284+Yu-Shi-Dre@users.noreply.github.com>
2025-06-30 21:09:08 +0700

    Update bt14.pas

--- Danh Sách Bài Tập/bt14.pas
+++ Danh Sách Bài Tập/bt14.pas
@@ -5,7 +5,8 @@ var a,b,c,d:real;
 begin
     clrscr;
     writeln('Tinh trung binh cong cua 4 so.');
-    writeln('Nhap 4 so : ');readln(a);readln(b);readln(c);readln(d);
+    writeln('Nhap 4 so : ');
+    readln(a);readln(b);readln(c);readln(d);
     writeln;
     write('Trung binh cong la: ',(a+b+c+d)/4:0:2);
     readln
