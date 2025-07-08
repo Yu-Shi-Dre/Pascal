d4b6c14d5c22b38d992c326bd160a0cd8a822435
Nee Yu Shii <85595284+Yu-Shi-Dre@users.noreply.github.com>
2025-07-08 13:48:42 +0700

    Update bt20.pas

--- Danh Sách Bài Tập/bt20.pas
+++ Danh Sách Bài Tập/bt20.pas
@@ -7,7 +7,7 @@ begin
     write('Tinh dien tich hinh tron. Ban hay nhap ban kinh: ');readln(r);
     if r>o then
      begin
-        s := pi*sqr(r);
+        s := pi*r*r;
         write('Dien tich hinh tron la: ',s:4:2);
      end
     else write('Ban da nhap sai so ! Vui long chay lai chuong trinh');
