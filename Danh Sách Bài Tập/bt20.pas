c05c894f27d22cac2ac2d0079798f32296b816c2
Nee Yu Shii <85595284+Yu-Shi-Dre@users.noreply.github.com>
2025-07-08 13:47:52 +0700

    Update bt20.pas

--- Danh Sách Bài Tập/bt20.pas
+++ Danh Sách Bài Tập/bt20.pas
@@ -1,12 +1,15 @@
 {Tính diện tích hình tròn }
 program bt2;
-uses crt;
-var r:real;
-    s:real;
-const pi=3.14;
+uses crt,math;
+var r,s:real;
 begin
+    clrscr;
     write('Tinh dien tich hinh tron. Ban hay nhap ban kinh: ');readln(r);
-    s := pi*sqr(r);
-    write('Dien tich hinh tron la: ',s:4:2);
+    if r>o then
+     begin
+        s := pi*sqr(r);
+        write('Dien tich hinh tron la: ',s:4:2);
+     end
+    else write('Ban da nhap sai so ! Vui long chay lai chuong trinh');
     readln
 end.
