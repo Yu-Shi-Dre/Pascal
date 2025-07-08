3b94bdee57998da75b4df59797c4ab46abf66616
Nee Yu Shii <85595284+Yu-Shi-Dre@users.noreply.github.com>
2025-07-08 13:54:41 +0700

    Update bt19.pas

--- Danh Sách Bài Tập/bt19.pas
+++ Danh Sách Bài Tập/bt19.pas
@@ -4,18 +4,17 @@ uses crt;
 var a,b,c:real;
 begin
     clrscr;
-    writeln('Kiem tra co phai tam giac deu khong .'+#13#10+#13#10,'Nhap do dai ba canh lan luot la: ');
+    writeln('Kiem tra co phai tam giac deu khong .');
+    write('Nhap do dai ba canh lan luot la: ');
     readln(a);readln(b);readln(c);
     writeln;
-    if ((a+b)<c) or ((a+c)<b) or ((b+c)<a) then write('Ban da nhap sai so !')
-    else
-      begin
-         if (a*b*c<=0) then write('Ban da nhap sai so !')
-         else
+    if (a*b*c)>0 and ((a+b)>c) and ((a+c)>b) and ((b+c)>a) then 
            begin
-              if (a=b) and (a=b) and (a=c) then write('Day la tam giac deu !')
+              if (a=b) and (a=b) and (a=c) then 
+              write('Day la tam giac deu !');
               else write('Day khong phai tam giac deu !');
-          end;
+           end
+    else write('Ban da nhap sai so ! Vui long chay lai chuong trinh');
     end;
     readln
 end.
